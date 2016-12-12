fa32a809fa5eeb44d2fe2ec8bb98abf2e22393cf
Donni Richasdy <donni.richasdy@gmail.com>
2016-12-12 16:21:35 +0700

    python packege

---
+++ pyPackage/G3.py
@@ -0,0 +1,9 @@
+#!/usr/local/bin/python3.5
+'''
+Created on Sep 8, 2016
+
+@author: richasdy
+'''
+
+def G3():
+   print "I'm G3 Phone"
---
+++ pyPackage/Isdn.py
@@ -0,0 +1,10 @@
+#!/usr/local/bin/python3.5
+'''
+Created on Sep 8, 2016
+
+@author: richasdy
+'''
+#!/usr/bin/python
+
+def Isdn():
+   print "I'm Isdn Phone"
